c2fb95ce0c784739640ec0b8467d26a93b1f481a
Soulriv <darkstarsoulriv@gmail.com>
2019-08-22 17:15:45 +0300

    [+] Dashboard - Fixed close icon in setup

--- components/dashboard/SetupCard.vue
+++ components/dashboard/SetupCard.vue
@@ -1,31 +1,37 @@
 <template>
   <v-card>
     <v-card-text>
-      <v-layout row wrap align-center>
-        <v-flex sm1>
-          <v-icon class="dashboard__setup-avatar" large dark>
-            person
-          </v-icon>
-        </v-flex>
-        <v-flex sm2>
-          Set-Up in Progress
-        </v-flex>
-        <div class="dashboard__progress-container">
-          <v-progress-linear v-model="currentProgress" class="dashboard__progress-bar" />
-        </div>
-        <v-flex sm2>
-          <v-btn
-            flat
-            round
-            class="text-none azure base homepage__btn white--text ml-0"
-          >
-            Continue Set-Up
-          </v-btn>
+      <v-layout row wrap align-start>
+        <v-flex xs11>
+          <v-layout row wrap align-center>
+            <v-flex xs3 sm2>
+              <v-icon class="dashboard__setup-avatar" large dark>
+                person
+              </v-icon>
+            </v-flex>
+            <v-flex sm2>
+              Set-Up in Progress
+            </v-flex>
+            <v-flex class="dashboard__progress-container">
+              <v-progress-linear v-model="currentProgress" class="dashboard__progress-bar" />
+            </v-flex>
+            <v-flex xs12 sm2>
+              <v-btn
+                flat
+                round
+                class="text-none azure base homepage__btn white--text ml-0"
+              >
+                Continue Set-Up
+              </v-btn>
+            </v-flex>
+          </v-layout>
         </v-flex>
         <v-spacer />
-        <v-icon @click="closeCard">
-          close
-        </v-icon>
+        <v-flex xs1 class="text-xs-right">
+          <v-icon @click="closeCard">
+            close
+          </v-icon>
+        </v-flex>
       </v-layout>
     </v-card-text>
   </v-card>
