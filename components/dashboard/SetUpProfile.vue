c0465311f658f4b3ecc1914ec4cab6275f3929ea
YuraGodlevskiy <godlevskiy.yuriy@gmail.com>
2019-09-03 17:31:59 +0300

    update(dashboard/freelancer): add general layout TODO add correct adaptive design + add 2 new components

---
+++ components/dashboard/SetUpProfile.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-card class="dashboard__card base-card-shadow">
+    <v-card-text class="base-card-shadow">
+      <v-layout row wrap align-center justify-center>
+        <v-flex xs12 sm12 md1 lg1 text-sm-center>
+           <v-img
+             :src="starImg"
+             :height="24"
+             :width="24"
+           />
+        </v-flex>
+        <v-flex xs12 sm12 md6 lg6 text-sm-center>
+          Set Up your Profile Today and Get +10 Impacts
+        </v-flex>
+        <v-flex xs12 sm12 md3 text-sm-center>
+          <v-btn
+            flat
+            round
+            class="text-none azure base homepage__btn white--text ml-0"
+          >
+            Start Set-Up
+          </v-btn>
+        </v-flex>
+        <div class="setup-profile__close-btn">
+          <v-icon @click="closeCard">
+            close
+          </v-icon>
+        </div>
+      </v-layout>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import starImg from '../../assets/images/dashboard/star.png';
+
+export default {
+  name: 'SetUpProfile',
+  data() {
+    return {
+      starImg
+    };
+  },
+  methods: {
+    closeCard() {
+      this.$emit('closeSetup');
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
