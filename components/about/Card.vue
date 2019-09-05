d3b2078ece7f8ef11393bf0fd99cf3438851bd24
YuraGodlevskiy <godlevskiy.yuriy@gmail.com>
2019-09-05 11:18:53 +0300

    add about page + first block

---
+++ components/about/Card.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="base-card base-card_default base-card-shadow">
+    <v-layout justify-center align-center>
+      <v-flex xs10>
+        <h2 v-if="header" class="about__header">
+          {{ header }}
+        </h2>
+        <h3 v-if="title">
+          {{ title }}
+        </h3>
+        <div v-if="content" class="about__content">
+          <p v-for="contentTxt in content" :key="contentTxt.id" class="about__content--txt">
+            {{ contentTxt }}
+          </p>
+        </div>
+      </v-flex>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    header: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    content: {
+      type: Array,
+      default: () => []
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="about">
+    <v-layout justify-center align-center>
+      <v-flex xs9>
+        <card
+          :header="aboutWorkImpact.header"
+          :content="aboutWorkImpact.content"
+        />
+      </v-flex>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+import Card from '@/components/about/Card';
+
+export default {
+  name: 'About',
+  components: {
+    Card
+  },
+  data() {
+    return {
+      aboutWorkImpact: {
+        header: 'About Work for Impact',
+        content: [
+          'Work for Impact is a freelance platform designed specifically for socially, environmentally responsible and nonprofit organizations. We offer businesses access to a talented workforce, and provide exciting paid work opportunities to freelancers.',
+          'By connecting these organizations with skilled individuals, we aim to bring life to meaningful projects that will have a positive impact on our world.'
+        ]
+      }
+    };
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
